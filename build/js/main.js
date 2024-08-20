1d2ba5749310fb434e7fc239952619a3031ebff9
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-08-20 17:49:51 +0600

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let username = "Masud sarder";
+console.log(username);
+let a = 23;
+let b = "3";
+let c = 3;
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+let username = "Masud sarder"
+console.log(username);
+
+
+let a = 23
+let b = "3"
+let c = 3
