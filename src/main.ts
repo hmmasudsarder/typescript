1544ca7122ece4bd2aa0632355d873d2ffd1e773
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-08-22 23:45:10 +0600

    added array

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
-let myName = "Masud"
+// assign a variable 
+// let myName = "Masud"
 
-let number: = 23
+// let number: = 23
+
+
+// assign a Array 
+
+let myNameList: [string, boolean] = ["masud", true]
+
+console.log(myNameList);
